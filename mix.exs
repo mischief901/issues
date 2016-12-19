f95e093263173f9c013f1459cf6783c52b3c571d
mischief901 <amisch01@linux.cs.tufts.edu>
2016-12-19 17:32:39 -0500

    finished working through example problem.

--- mix.exs
+++ mix.exs
@@ -3,10 +3,13 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     name: "Issues",
+     source_url: "https://github.com/mischief901/issues",
      deps: deps()]
   end
 
@@ -14,7 +17,7 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [ :logger, :httpoison ] ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +30,15 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      httpoison: "~> 0.9",
+      poison: "~> 2.2",
+      ex_doc: "~> 0.12",
+      earmark: "~> 1.0"
+    ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
